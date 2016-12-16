aa7e7db802e6be5600cb48d9c7195ef6127d15a6
MadalynBotkin <magdalena.botkin@gmail.com>
2016-12-16 06:31:05 -0700

    Update StringArray.java

--- StringArray.java
+++ StringArray.java
@@ -1,6 +1,6 @@
 //Madalyn Botkin
 //StringArray.java
-//
+//Strings that the user enters will stored in an array, then the longest, shortest and alphabetically first strings will be outputted, along with their information.
 
 import java.util.Scanner;
 	
@@ -20,6 +20,15 @@ public class StringArray
 		
 		String stringArray[];	//the string array
 		int numStrings;
+		int index = 0;
+		int longStringIndex = 0;
+		int shortStringIndex = 0;
+		int firstStringIndex = 0;
+		String longerString = "";
+		String shorterString = "";
+		String firstString = "";
+		String compareString = "";
+
 		
 		
 		System.out.print("How many strings do you want to enter?: ");
@@ -37,7 +46,51 @@ public class StringArray
 		
 		
 		//find the longest string
-		for (; ; )
-		//traverse
+		for (index = 0; index < stringArray[].length; index++)
+		{
+			longerString = stringArray[index];
+			compareString = stringArray[index + 1];
+			
+			if (compareString > longerString)	//if the compareString is found to be longer, it becomes longerString
+			{
+				longerString = compareString;
+				longStringIndex = index;	//keep track of the index of the longest string
+			}
+		}
+		//output the longest string's info
+		System.out.print("\n\nLongest string: " + longerString + "\nLength: " + stringArray[longStringIndex].length + "\nIndex Position: " + longStringIndex);
+
+		
+		
+		//find the shortest string
+		for (index = 0; index < stringArray[].length; index++)
+		{
+			shorterString = stringArray[index];
+			compareString = stringArray[index + 1];
+			
+			if (compareString < shorterString)	//if the compareString is found to be shorter, it becomes shorterString
+			{
+				longerString = compareString;
+				shortStringIndex = index;	//keep track of the index of the shortest string
+			}
+		}
+		//output the shortest string's info
+		System.out.print("\n\nShortest string: " + shorterString + "\nLength: " + stringArray[shortStringIndex].length + "\nIndex Position: " + shortStringIndex);
+
+		
+		//find the string that comes first alphabetically
+		for (; index < stringArray[].length; index++)
+		{
+			String firstString = stringArray[index];
+			String compareString = stringArray[index + 1];
+			
+			if (compareString.compareTo(longerString) > 0)	//if the compareString is l
+			{
+				longerString = compareString;
+				firstStringIndex = index;	//keep track of the index of the first string
+			}
+		}
+		//output the first string's info
+		System.out.print("\n\nAlphabetically first string: " + firstString + "\nLength: " + stringArray[firstStringIndex].length + "\nIndex Position: " + firstStringIndex);
 	}
 }
