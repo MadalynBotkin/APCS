d37a2478e97d3352d8332f80a5bc783803bf8561
MadalynBotkin <magdalena.botkin@gmail.com>
2017-02-07 22:54:47 -0700

    Update EmployeeTest.java

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -144,6 +144,16 @@ class HourlyEmp extends Employee
 	
 	//methods
 	
+	public void setHourlyWage(double hourlyWage)
+	{
+		this.hourlyWage = hourlyWage;
+	}
+	
+	public void setHoursPerWeek(int hoursPerWeek)
+	{
+		this.hoursPerWeek = hoursPerWeek;
+	}
+	
 	public double calcPay()
 	{
 		return hourlyWage * hoursPerWeek;
@@ -169,17 +179,34 @@ class CommissionEmp extends Employee
 	}
 	
 	//constructor that accepts the variables as parameters
-	public CommissionEmp(String firstName, String lastName, int SSN)
+	public CommissionEmp(String firstName, String lastName, int SSN, double commRate, double weeklyGrossSales)
 	{
-		super();
+		super(firstName, lastName, SSN);
+		this.commRate = commRate;
+		this.weeklyGrossSales = weeklyGrossSales;
 	}
 	
 	
 	//methods
 	
+	public void setCommRate(double commRate)
+	{
+		this.commRate = commRate;
+	}
+	
+	public void setWeeklyGrossSales(double weeklyGrossSales)
+	{
+		this.weeklyGrossSales = weeklyGrossSales;
+	}
+	
 	public double calcPay()
 	{
 		return weeklyGrossSales * commRate;
+	}
+	
+	public String toString()	//return all of its information
+	{
+		return "Name: " + super.getFirstName() + " " + super.getLastName() + "\nSocial Security Number: " + super.getSSN() + "\nPay: $" + calcPay() + "\n";
 	}
 }//end of CommisionEmp
 
@@ -217,7 +244,8 @@ public class EmployeeTest
 {
 	public static void main(String [] args)
 	{
-		Employee salEmp1, salEmp2, hourEmp1, hourEmp2;
+		Employee salEmp1, salEmp2, hourEmp1, hourEmp2, commEmp1, commEmp2;
+		CommissionEmp commPlusBaseEmp1, commPlusBaseEmp2;
 		
 		//salaried employees
 		
@@ -229,20 +257,51 @@ public class EmployeeTest
 		((SalariedEmp)salEmp1).setSSN(123456789);
 		((SalariedEmp)salEmp1).setWeeklySalary(23.0);
 		
-		System.out.print("Salaried Employees:\n\n" + salEmp1.toString());
+		System.out.print("\n\nSalaried Employees:\n\n" + salEmp1.toString());
 		System.out.print("\n" + salEmp2.toString());
+		
 		
 		//hourly employees
 		
 		hourEmp1 = new HourlyEmp();	//using the default constructor
-		hourEmp2 = new HourlyEmp("Gravy", "Train", 121212123, 15, 200);	//using constructor with parameters
+		hourEmp2 = new HourlyEmp("Mr. Gravy", "Train", 121212123, 17, 2000);	//using constructor with parameters
 		
-		salEmp1.setFirstName("Janice");
-		salEmp1.setLastName("Smith");
-		((SalariedEmp)salEmp1).setSSN(112358132);
-		((SalariedEmp)salEmp1).setWeeklySalary(23.0);
+		hourEmp1.setFirstName("Janice");
+		hourEmp1.setLastName("Smith");
+		((HourlyEmp)hourEmp1).setSSN(112358132);
+		((HourlyEmp)hourEmp1).setHourlyWage(10.0);
+		((HourlyEmp)hourEmp1).setHoursPerWeek(30);
 		
-		System.out.print("Salaried Employees:\n\n" + salEmp1.toString());
-		System.out.print("\n" + salEmp2.toString());
+		System.out.print("\n\n\nHourly Employees:\n\n" + hourEmp1.toString());
+		System.out.print("\n" + hourEmp2.toString());
+		
+		
+		//commission employees
+		
+		commEmp1 = new CommissionEmp();	//using the default constructor
+		commEmp2 = new CommissionEmp("John", "Watson", 132333435, 20.75, 14.50);	//using constructor with parameters
+		
+		commEmp1.setFirstName("Erik");
+		commEmp1.setLastName("Satie");
+		((CommissionEmp)commEmp1).setSSN(186619250);
+		((CommissionEmp)commEmp1).setCommRate(7.50);
+		((CommissionEmp)commEmp1).setWeeklyGrossSales(20);
+		
+		System.out.print("\n\n\nCommission Employees:\n\n" + commEmp1.toString());
+		System.out.print("\n" + commEmp2.toString());
+		
+		//base plus commission employees
+		
+		commPlusBaseEmp1 = new CommissionPlusBaseEmp();	//using the default constructor
+		commPlusBaseEmp2 = new CommissionPlusBaseEmp("Queen", "Elizabeth", 311748314, 50, 14.50);	//using constructor with parameters
+		
+		commPlusBaseEmp1.setFirstName("Mary");
+		commPlusBaseEmp1.setLastName("Watson");
+		((CommissionEmp)commPlusBaseEmp1).setSSN(171714874);
+		((CommissionEmp)commPlusBaseEmp1).setCommRate(7.50);
+		((CommissionEmp)commPlusBaseEmp1).setWeeklyGrossSales(20);
+		
+		System.out.print("\n\n\nCommission Employees:\n\n" + commPlusBaseEmp1.toString());
+		System.out.print("\n" + commPlusBaseEmp2.toString());
 	}
 }
