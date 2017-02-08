78cb8fdab6ae19ad21d2c7684eba9969a415d991
MadalynBotkin <magdalena.botkin@gmail.com>
2017-02-08 08:59:19 -0700

    Update ExceptionEngine.java

--- ExceptionEngine.java
+++ ExceptionEngine.java
@@ -8,6 +8,7 @@ public class ExceptionEngine
 	{
 		int input = 0;
 		
+		//the menu
 		while (input != 4)
 		{
 			System.out.print("1) Null Pointer Exception\n2) Array Index Out Of Bounds Exception\n3) Arithmetic Exception\n4) Quit");
@@ -21,12 +22,21 @@ public class ExceptionEngine
 	}//end of main
 	
 	//determines whether the JVM should abort or handle the exception
-		public static boolean abortOrHandle(String exceptionDescription)
+	public static boolean abortOrHandle(String exceptionDescription)
+	{
+		String exceptDesc;	//the exception description
+		exceptDesc = exceptionDescription;
+		
+		if (!exceptionDescription.equals("NullPointer") && !exceptionDescription.equals("ArrayIndex"))
 		{
-			if (!(exceptionDescription.equals(“NullPointer”) && exceptionDescription.equals(“ArrayIndex”)))
-			{
-				System.out.print("");
-				return false;
-			}
+			System.out.print("");
+			return false;
 		}
+		
+		else if (exceptionDescription.equals("NullPointer") || exceptionDescription.equals("ArrayIndex"))
+		{
+			System.out.print("");
+			//return false;
+		}
+	}
 }
