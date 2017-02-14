9abebf179a453ead614fc3e47455a3c273b97475
MadalynBotkin <magdalena.botkin@gmail.com>
2017-02-14 09:06:39 -0700

    Create BinSen.java

---
+++ BinSen.java
@@ -0,0 +1,13 @@
+//Madalyn Botkin
+//BinSen.java
+//
+
+public class BinSen
+{
+	public static void main(String [] args)
+	{
+		int[] array = new int[15];
+		int ranNum = Random.nextInt() * 10 + 1;
+		
+	}
+}
