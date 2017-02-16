e2129cb7cd9f8a4bff785cf3ae4325fd7126ac9e
MadalynBotkin <magdalena.botkin@gmail.com>
2017-02-15 21:04:05 -0700

    Update ExceptionEngine.java

--- ExceptionEngine.java
+++ ExceptionEngine.java
@@ -13,6 +13,7 @@ public class ExceptionEngine
 		int input = 0;
 		int numerator = 0;
 		int denominator = 0;
+		double quotient = 0;
 		
 		//the menu
 		while (input != 4)
@@ -31,10 +32,23 @@ public class ExceptionEngine
 					numerator = reader.nextInt();
 					System.out.print("Enter denominator: ");
 					denominator = reader.nextInt();
+					
+					try	//Arithmetic
+						{
+							quotient = numerator / denominator;
+						}
+						catch(ArithmeticException e)
+						{
+							System.out.print("\n1) Handle the ArimeticException\n2) Abort\n\n");
+							input =  reader.nextInt();
+						}
 			}
 			try {java.lang.Thread.sleep(100);} catch(Exception e) {}
 		}
 	}//end of main
+	
+	
+	
 	
 	//determines whether the JVM should abort or handle the exception
 	public static boolean abortOrHandle(String exceptionDescription)
@@ -45,7 +59,7 @@ public class ExceptionEngine
 		
 		if (!exceptionDescription.equals("NullPointer") && !exceptionDescription.equals("ArrayIndex"))
 		{
-			System.out.print("Shamalamadingdong!");
+			System.out.print("Warning: There is an exception here.");
 			return false;
 		}
 		
@@ -56,16 +70,33 @@ public class ExceptionEngine
 			
 			switch (input)
 			{
-				case 1:
-					try
+				case 1:	//user wants to handle exception
+					if (exceptionDescription.equals("NullPointer"))
 					{
-						String nullString = null;
-						System.out.print(nullString.length());//causes a null pointer exception
+						try	//NullPointer
+						{
+							String nullString = null;
+							System.out.print(nullString.length());//causes a null pointer exception
+						}
+						catch(Exception e)
+						{
+							System.out.print("Uh-oh. There's a null pointer exception here.\n\n");
+						}
 					}
-					catch(Exception e)
+					
+					if (exceptionDescription.equals("ArrayIndex"))
 					{
-						System.out.print("Uh-oh. There's a null pointer exception here.\n\n");
+						try	//ArrayIndex
+						{
+							String nullString = null;
+							System.out.print(nullString.length());//causes a null pointer exception
+						}
+						catch(Exception e)
+						{
+							System.out.print("Uh-oh. There's an array index exception here.\n\n");
+						}
 					}
+					
 					return true;
 					
 				case 2:
