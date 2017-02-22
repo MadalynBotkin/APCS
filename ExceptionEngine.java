5417b27bff83422e80e80f6fa32a49022a9b6f5a
MadalynBotkin <magdalena.botkin@gmail.com>
2017-02-21 18:49:48 -0700

    Update ExceptionEngine.java

--- ExceptionEngine.java
+++ ExceptionEngine.java
@@ -1,6 +1,8 @@
 //Madalyn Botkin
 //ExceptionEngine.java
-//
+//User has 3 different exceptions to choose from: NullPointer, ArrayIndexOutOfBounds, and Arithmetic. If either of the first two is chosen, the abortOrHandle method is called,
+//	and in it the user must decide to abort or handle the exception. In the third option, if there is an Arithmetic Exception, user is prompted for new numbers, and those numbers and
+//	their quotient are outputted.
 
 import java.util.Scanner;
 
@@ -25,29 +27,31 @@ public class ExceptionEngine
 			{
 				case 1:
 					abortOrHandle("NullPointer");
+					break;
 				case 2:
 					abortOrHandle("ArrayIndex");
+					break;
 				case 3:
 					System.out.print("\nEnter numerator: ");
 					numerator = reader.nextInt();
 					System.out.print("Enter denominator: ");
 					denominator = reader.nextInt();
 					
-					try	//Arithmetic
+					try	//ArithmeticException
 						{
 							quotient = numerator / denominator;
 						}
 						catch(ArithmeticException e)
 						{
-							System.out.print("\n1) Handle the ArimeticException\n2) Abort\n\n");
-							input =  reader.nextInt();
-							
-							switch (input)
-							{
-								case 1:
-									
-							}
+							//the user enters a new numerator and denominator
+							System.out.print("\n\n\nUh-oh. There is an arithmetic exception here.\n\nEnter new numerator: ");
+							numerator = reader.nextInt();
+							System.out.print("Enter new denominator: ");
+							denominator = reader.nextInt();
 						}
+						quotient = (double)numerator / (double)denominator;	//typecast to double so it does the math right
+						System.out.print("Numerator: " + numerator + "\nDenominator: " + denominator + "\nQuotient: " + quotient + "\n\n");	//output all the information
+					break;
 			}
 		}
 	}//end of main
@@ -62,6 +66,7 @@ public class ExceptionEngine
 		
 		int choice = 0;
 		
+		//if it's neither a NullPointer or ArrayIndex
 		if (!exceptionDescription.equals("NullPointer") && !exceptionDescription.equals("ArrayIndex"))
 		{
 			System.out.print("Warning: There is an exception here.");
@@ -75,7 +80,7 @@ public class ExceptionEngine
 			
 			switch (input)
 			{
-				case 1:	//user wants to handle exception
+				case 1:	//user wants to handle exception (output an error message)
 					if (exceptionDescription.equals("NullPointer"))
 					{
 						try	//NullPointer
@@ -101,10 +106,8 @@ public class ExceptionEngine
 							System.out.print("Uh-oh. There's an array index exception here.\n\n");
 						}
 					}
-					
-					return true;
-					
-				case 2:
+					break;
+				case 2:	//cause an exception to stop the program from executing
 					if (exceptionDescription.equals("NullPointer"))
 					{
 						String nullString = null;
@@ -118,6 +121,7 @@ public class ExceptionEngine
 						System.out.print(array[4]);//causes an array index out of boundse exception 
 						return false;
 					}
+					break;
 			}
 		}
 			return false;
