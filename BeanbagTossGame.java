fe27a4b5cb72753cfe6b7542d77a48d40ff72c2e
MadalynBotkin <magdalena.botkin@gmail.com>
2017-03-12 17:46:24 -0600

    Update BeanbagTossGame.java

--- BeanbagTossGame.java
+++ BeanbagTossGame.java
@@ -8,4 +8,44 @@ class Beanbag
 	private	int myCol;
 	private int myValue;
 	private boolean activated;
+	
+	
+	
+	//default constructor (used for creating a matrix of blank beanbags when the program begins)
+	Beanbag()
+	{
+		myRow = 0;
+		myCol = 0;
+		myValue = 0;
+		activated = false;
+	}
+		
+	//constructor that accepts all the variables (the actual beanbags that the user throws)
+	Beanbag(int incomingRow, int incomingCol, int incomingValue)
+	{
+		myRow = incomingRow;
+		myCol = incomingCol;
+		myValue = incomingValue;
+		activated = false;
+	}
+	
+	
+	
+	
+	//methods
+	
+	int getValue()	//returns the beanbag's value in a cell
+	{
+		return myValue;
+	}
+	
+	void hitBeanbag()	//sets activated to true
+	{
+		activated = true;
+	}
+	
+	boolean isActivated()	//returns whether the beanbag is activated or not
+	{
+		return activated;
+	}
 }
