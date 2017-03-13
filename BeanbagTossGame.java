293c20fc1e1ebb96d6cb3b091ad38dd90f0a5973
MadalynBotkin <magdalena.botkin@gmail.com>
2017-03-12 20:23:44 -0600

    Update BeanbagTossGame.java

--- BeanbagTossGame.java
+++ BeanbagTossGame.java
@@ -49,3 +49,104 @@ class Beanbag
 		return activated;
 	}
 }
+
+
+
+
+
+
+
+public class BeanbagTossGame
+{
+	public static void main (String [] args)
+	{
+		char input = 'i';
+		int r = 0;
+		int c = 0;
+		
+		//make the beanbag array
+		Beanbag[][] beanbagArray = new Beanbag[5][5];
+		
+		//fill the array with beanbags
+		for (; r < beanbagArray.length; r++)
+		{
+			//first and last row
+			if (r == 0 || r == 4)
+			{
+				for (c = 0; c < beanbagArray[0].length; c++)
+				{
+					beanbagArray[r][c] = new Beanbag(r, c, 1);
+				}
+			}
+			
+			//second and fourth row
+			if (r == 1 || r == 3)
+			{
+				for (c = 0; c < beanbagArray[0].length; c++)
+				{
+					if (c == 0 || c == 4)
+					{
+						beanbagArray[r][c] = new Beanbag(r, c, 1);
+					}
+			
+					else
+					{
+						beanbagArray[r][c] = new Beanbag(r, c, 2);
+					}
+			
+				}
+			}
+			
+			//third row
+			if (r == 2)
+			{
+				for (c = 0; c < beanbagArray[0].length; c++)
+				{
+					if (c == 0 || c == 4)
+					{
+						beanbagArray[r][c] = new Beanbag(r, c, 1);
+					}
+					
+					if (c == 1 || c == 3)
+					{
+						beanbagArray[r][c] = new Beanbag(r, c, 2);
+					}
+			
+					else if (c == 2)
+					{
+						beanbagArray[r][c] = new Beanbag(r, c, 3);
+					}
+			
+				}
+			}
+		}	//end of row loop
+		
+		
+		
+		
+		
+		
+		
+		//output the array////////////////////////////////////////////////////put in while loop
+		for (r = 0; r < beanbagArray.length; r++)
+		{
+			for (c = 0; c < beanbagArray[0].length; c++)
+			{
+				System.out.print(beanbagArray[r][c].getValue() + "\t");
+			}
+			System.out.print("\n");
+		}
+		
+		
+		
+		
+		
+		
+		
+		//menu
+		while (input != 'q' && input != 'Q')
+		{
+			System.out.print("");
+		}
+	}//end of main
+}
