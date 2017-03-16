9c1fe2019a206d59f2c7530509f563840b4f7409
MadalynBotkin <magdalena.botkin@gmail.com>
2017-03-15 19:37:05 -0600

    Create IntListInsert.java

---
+++ IntListInsert.java
@@ -0,0 +1,14 @@
+//Madalyn Botkin
+//IntListInsert.java
+//
+
+
+import java.util.ArrayList;
+
+class IntListInsert
+{
+	public static void main (String [] args)
+	{
+		List<Integer> list = new ArrayList<Integer>();
+	}
+}
