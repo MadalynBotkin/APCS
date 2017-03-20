8b1ff7d69d3ca43dd8574dc83c1ab67fea61bf18
MadalynBotkin <magdalena.botkin@gmail.com>
2017-03-20 09:16:41 -0600

    Update CharCount.java

--- CharCount.java
+++ CharCount.java
@@ -13,6 +13,13 @@ class charCount
 		ArrayList<Integer> numCharList = new ArrayList<Integer>();	//the number of times each character occurs
 		String text;
 		Character charObj;
+		int index = 0;	//index of where you are in the string
+		int listIndex = -1;	//index of where you are in the ArrayLists
+		int listASCII;	//the ASCII value of a character in the ArrayList
+		int compareASCII;	//the ASCII value of the character that's being compared to elements in the ArrayList
+		int numCharCount = 0;
+		boolean duplicate = false;
+		Character charObj;
 	
 		System.out.print("Enter a sentence or even a paragraph: ");
 		text = reader.nextLine();
@@ -20,7 +27,62 @@ class charCount
 		text = text.toUpperCase();	//convert all characters to uppercase letters
 		System.out.print(text);
 		
-		for (int i = 0; i < )
+		for (; index < text.length(); index++)	//traverse string
+		{
+			compareASCII = (int) text.charAt(index);
+			charObj = new Character(text.charAt(index));	//convert char to Character wrapper object
+			numCharCount = 0;
+			
+			if (charList.size() == 0 && numCharList.size() == 0)	//if the lists are empty in the beginning
+			{
+				charList.add(text.charAt(index));
+				numCharList.add(listIndex, 1);
+			}
+			
+			else	//if not empty
+			{
+				
+				
+				
+				listIndex = -1;
+				duplicate = false;
+				
+				//find a place to put the char
+				for (int i = 0; i < charList.size(); i++)
+				{
+					if (listIndex == -1 && duplicate == false)	//if a place nor duplicate are found yet
+					{
+						if ( < )	//found a place///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+						{
+							listIndex = i;	//keep track of the index of where to place intObj
+						}
+						
+						if (intObj.intValue() == list.get(i).intValue())	//flag if it's a duplicate
+						{
+							duplicate = true;
+						}
+					}
+				}
+					
+				if (duplicate == false)	//don't add duplicates
+				{
+					if (placeIndex == -1)	//if it's the largest integer yet, put it at the end
+					{
+						list.add(intObj);
+					}
+				
+					else	//a place was found to put it
+					{
+						list.add(placeIndex, intObj);
+					}
+
+				
+				
+				
+				
+			}
+			
+		}//end of for loop
 	}
 }
 
@@ -32,3 +94,15 @@ class charCount
 /*I went to the grocery store and bought so many turkeys I couldn't fit them all in the cart. Not to mention I bought two thousand cans of gravy to go along with the meal.
  *i = where you are in both arrayLists
  *index = the index of where you are in the string
+ *
+ *get character
+ *get ascii value
+ *compare value to others
+ *sort like IntListInsert
+ *
+ *
+ *
+ *
+ *
+ *
+ */
