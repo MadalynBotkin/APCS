b2dbea5434958668bdb2c807547b203d76ac0ee6
MadalynBotkin <magdalena.botkin@gmail.com>
2017-03-19 19:46:19 -0600

    Update IntListInsert.java

--- IntListInsert.java
+++ IntListInsert.java
@@ -1,6 +1,6 @@
 //Madalyn Botkin
 //IntListInsert.java
-//
+//There is an arrayList of Integers. The user specifies how many integers to be entered into the arrayList, and then enters them. As the integers are being entered, they are placed in order, but no duplicates are added to the arrayList.
 
 
 import java.util.*;
@@ -10,56 +10,90 @@ class IntListInsert
 	public static void main (String [] args)
 	{
 		Scanner reader = new Scanner(System.in);
-		List<Integer> list = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		int numInts = 0;	//number of integer the user wants to enter
 		int num = 0;
+		int placeIndex = -1;
 		Integer intObj;
 		boolean inserted = false;
+		boolean duplicate;
 		
 		//find out how many integers the user wants to enter
 		System.out.print("How many integers do you want to enter?: ");
 		numInts = reader.nextInt();
 		
-		//enter in the integers
-		while (numInts != 0)
+		
+		
+		
+		
+		
+		
+		
+		//enter the integers
+		while (numInts > 0)
 		{
-			System.out.print("\n\nEnter an integer: ");
+			System.out.print("\nEnter an integer: ");
 			num = reader.nextInt();
 			
 			intObj = new Integer(num);	//turn the int into an Integer wrapper object
 			
 			inserted = false;
-			int i = 0;
 			
-			while (inserted == false)
+			if (list.size() == 0)	//if the list is empty in the beginning
 			{
-				if (intObj < list.get(i))
-				{
-					list.add(i, intObj);
-					inserted = true;	//so the it doesn't get inserted multiple times
-					numInts--;
-				}
-				
-				else if (intObj != list.get(i))
-				{
-					list.add(intObj);
-				}
-				
-				i++;
-			}
+				list.add(intObj);
+			}	
 			
-			/*put it in the array list
-			for (int i : list)
+			else	//if not empty
 			{
-				if (num < i && inserted == false)
+				placeIndex = -1;
+				duplicate = false;
+				
+				//find a place to put the integer
+				for (int i = 0; i < list.size(); i++)
 				{
+					if (placeIndex == -1 && duplicate == false)	//if a place nor duplicate are found yet
+					{
+						if (intObj.intValue() < list.get(i).intValue())	//found a place
+						{
+							placeIndex = i;	//keep track of the index of where to place intObj
+						}
+						
+						if (intObj.intValue() == list.get(i).intValue())	//flag if it's a duplicate
+						{
+							duplicate = true;
+						}
+					}
+				}
 					
-					inserted = true;	//so num doesn't get inserted multiple times
-					numInts--;
+				if (duplicate == false)	//don't add duplicates
+				{
+					if (placeIndex == -1)	//if it's the largest integer yet, put it at the end
+					{
+						list.add(intObj);
+					}
+				
+					else	//a place was found to put it
+					{
+						list.add(placeIndex, intObj);
+					}
 				}
-			}*/
-			
-			//System.out.print(inserted);
-		}//end of while loop
+				
+			}
+			numInts--;
+		}
+	
+		//output
+		System.out.print("\n\nIndex:\t\t");
+		for (int i = 0; i < list.size(); i++)	//output indices
+		{
+			System.out.print(i + "\t");
+		}
+		
+		System.out.print("\nInteger:\t");
+		for (Integer k : list)	//output integers
+		{
+			System.out.print(k + "\t");
+		}
 	}
 }
