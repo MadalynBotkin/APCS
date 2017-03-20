b2b9d85ed43b6bb863fee99339956a6aec6fc3b7
MadalynBotkin <magdalena.botkin@gmail.com>
2017-03-20 08:49:44 -0600

    Create CharCount.java

---
+++ CharCount.java
@@ -0,0 +1,34 @@
+//Madalyn Botkin
+//CharCount.java
+//
+
+import java.util.*;
+
+class charCount
+{
+	public static void main(String [] args)
+	{
+		Scanner reader = new Scanner(System.in);
+		ArrayList<Character> charList = new ArrayList<Character>();	//the characters in a string
+		ArrayList<Integer> numCharList = new ArrayList<Integer>();	//the number of times each character occurs
+		String text;
+		Character charObj;
+	
+		System.out.print("Enter a sentence or even a paragraph: ");
+		text = reader.nextLine();
+		
+		text = text.toUpperCase();	//convert all characters to uppercase letters
+		System.out.print(text);
+		
+		for (int i = 0; i < )
+	}
+}
+
+
+
+
+
+
+/*I went to the grocery store and bought so many turkeys I couldn't fit them all in the cart. Not to mention I bought two thousand cans of gravy to go along with the meal.
+ *i = where you are in both arrayLists
+ *index = the index of where you are in the string
