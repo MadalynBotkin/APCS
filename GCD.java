30876723f812c3f659f27d7cb335dcab54932970
MadalynBotkin <magdalena.botkin@gmail.com>
2017-05-12 07:53:11 -0600

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -2,13 +2,34 @@
 //GCD.java
 //
 
+import java.util.*;
 
 class GDC
 {
 	public static void main(System [] args)
 	{
+		Scanner reader = new Scanner(System.in);
+		int num1;
+		int num2;
 		
+		System.out.print("Enter an integer: ");
+		num1 = reader.nextInt();
+		System.out.print("Enter another one: ");
+		num2 = reader.nextInt();
 		
-		//send remainder
+		if (num2 > num1)
+		{
+			
+		}
+		
+		else
+		{
+			
+		}
+	}
+	
+	public static int findGCD(int num1, int num2)
+	{
+		return 42;
 	}
 }
