0d52046bb7377f59328b521051317f566a1bfbd8
MadalynBotkin <magdalena.botkin@gmail.com>
2017-05-12 08:28:36 -0600

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -1,12 +1,12 @@
 //Madalyn Botkin
 //GCD.java
-//
+//The user enters two integers. Those integers are sent to a method that finds the greatest common denominator between the two, then returns the answer.
 
 import java.util.*;
 
-class GDC
+public class GCD
 {
-	public static void main(System [] args)
+	public static void main (String [] args)
 	{
 		Scanner reader = new Scanner(System.in);
 		int num1;
@@ -19,17 +19,27 @@ class GDC
 		
 		if (num2 > num1)
 		{
-			
+			System.out.print(findGCD(num2, num1));	//the first integer in the parameters should be the greater number
 		}
 		
 		else
 		{
-			
+			System.out.print(findGCD(num1, num2));
 		}
 	}
 	
 	public static int findGCD(int num1, int num2)
 	{
-		return 42;
+		int remainder = num1 % num2;	//find the modulo of the integers
+		
+		if(remainder == 0)	//if the remainder is 0, then num2(the divisor) is the GCD
+		{
+			return num2;
+		}
+		
+		else
+		{
+			return findGCD(num2, remainder);	//keep sending the divisor and remainder until the GCD is reached
+		}
 	}
 }
